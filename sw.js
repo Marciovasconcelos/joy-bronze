9219283d36eb23d49686c17cc56dbf0e70192f10
akatsukydeliverysushi-coder <akatsuky.deliverysushi@gmail.com>
2026-09-08 23:08:48 -0300

    Adiciona service worker para modo aplicativo do Joy Bronze

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "joy-bronze-v1";
+const APP_SHELL = ["./", "./index.html", "./style.css", "./joy-bronze-concept.css", "./joy-header-fix.css", "./joy-layout-exato.css", "./cliente-publico.js", "./firebase-config.js", "./manifest.json"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match("./index.html"))));
+});
